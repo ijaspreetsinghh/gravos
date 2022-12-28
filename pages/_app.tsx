3f18c38743630433c69988028a8ed809ca9b9c12
Jaspreet <jaspreet.codrity@gmail.com>
2022-12-28 17:32:23 +0530

    first_block

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import localFont from "@next/font/local";
+import { Montserrat } from "@next/font/google";
+export const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
